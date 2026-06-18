e0f4fbc5dac8ef89004a8173fc002b2147235d9c
shivamyadv196 <shivamYadv196@gmail.com>
2026-06-18 19:00:20 +0530

    backend frontend connected

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ connectDB();
 app.use(express.json({ limit: "10mb" }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_PORT,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
